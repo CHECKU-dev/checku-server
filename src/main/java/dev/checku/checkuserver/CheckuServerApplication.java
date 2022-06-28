c7591fcf78e459fea07ed5485c9f5f0160a19764
lee-kyubong <46523628+bong01@users.noreply.github.com>
2022-06-28 16:48:42 +0900

    Init: 프로젝트 생성

---
+++ src/main/java/dev/checku/checkuserver/CheckuServerApplication.java
@@ -0,0 +1,13 @@
+package dev.checku.checkuserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckuServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckuServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/checku/checkuserver/CheckuServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.checku.checkuserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckuServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
