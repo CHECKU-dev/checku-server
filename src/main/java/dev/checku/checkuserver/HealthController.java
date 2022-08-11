d7680c767da4ff94734a3b00020235b9691e7f72
yoon <dudwls143@gmail.com>
2022-08-12 00:08:56 +0900

    feat: health controller 추가

---
+++ src/main/java/dev/checku/checkuserver/HealthController.java
@@ -0,0 +1,18 @@
+package dev.checku.checkuserver;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/health")
+public class HealthController {
+
+    @GetMapping("")
+    public String healthCheck() {
+        return "OK";
+    }
+
+}
