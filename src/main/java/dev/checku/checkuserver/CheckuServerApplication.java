3620ded7187b7b2406047a00925fdf727fabf738
yoon <dudwls143@gmail.com>
2022-08-18 15:41:48 +0900

    chore: 최신화

--- src/main/java/dev/checku/checkuserver/CheckuServerApplication.java
+++ src/main/java/dev/checku/checkuserver/CheckuServerApplication.java
@@ -3,7 +3,9 @@ package dev.checku.checkuserver;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @EnableEncryptableProperties
 @SpringBootApplication
 public class CheckuServerApplication {
