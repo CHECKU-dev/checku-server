22d4e0b883c7235a9bf78ea3b916f31c3f2b7826
yoon-youngjin <dudwls143@gmail.com>
2023-01-29 23:15:23 +0900

    feat : Logback 적용

--- src/main/java/dev/checku/checkuserver/HealthController.java
+++ src/main/java/dev/checku/checkuserver/HealthController.java
@@ -1,10 +1,12 @@
 package dev.checku.checkuserver;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/health")
@@ -12,6 +14,14 @@ public class HealthController {
 
     @GetMapping("")
     public String healthCheck() {
+
+        for (int i = 0; i < 100; i++) {
+            log.info("message Info");
+//            log.warn("message Warn");
+//            log.error("message Error");
+        }
+
+
 
         return "I'm OK...";
     }
