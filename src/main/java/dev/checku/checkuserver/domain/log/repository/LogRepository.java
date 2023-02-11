0ab38591e7ac135707d5fecc1c1af07506ec1fdc
lee-kyubong <46523628+bong01@users.noreply.github.com>
2023-02-11 16:10:25 +0900

    remove: 로그 도메인 제거

--- src/main/java/dev/checku/checkuserver/domain/log/repository/LogRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.checku.checkuserver.domain.log.repository;
-
-import dev.checku.checkuserver.domain.log.entity.Log;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LogRepository extends JpaRepository<Log, Long> , LogRepositoryCustom {
-}
