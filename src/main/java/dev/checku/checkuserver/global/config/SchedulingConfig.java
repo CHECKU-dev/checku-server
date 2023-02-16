02ab75564365742cb67eb45fd00556802ee64af8
yoon-youngjin <dudwls143@gmail.com>
2023-02-16 22:16:31 +0900

    feat : appender file 수정

--- src/main/java/dev/checku/checkuserver/global/config/SchedulingConfig.java
+++
@@ -1,9 +0,0 @@
-package dev.checku.checkuserver.global.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-
-@Configuration
-@EnableScheduling
-public class SchedulingConfig {
-}
